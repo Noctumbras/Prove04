c772aacd4b452451baa09210f35828f6e3c52b36
CaptainCarnivore <72416550+CaptainCarnivore@users.noreply.github.com>
2020-10-09 15:22:02 -0700

    hello!

---
+++ Main.java
@@ -0,0 +1,10 @@
+package prove04;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, my name is Stephen.");
+        System.out.println("Hello Stephen, my name is Austin.");
+    }
+}
